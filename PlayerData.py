0c52d2b02a8c884b313b5893bf61a8d2d558ae1c
michaelbarrack <michael@mbarrack.com>
2025-07-13 14:20:48 -0400

    Initial commit

---
+++ PlayerData.py
@@ -0,0 +1,11 @@
+import nfl_data_py as nfl
+import pandas as pd
+import numpy as  np
+
+
+def get_weekly_data() -> pd.DataFrame:
+    nfl.see_weekly_cols()
+    return pd.DataFrame
+
+
+get_weekly_data()
